dc9796332089b4910c96846a5ce8f0bce00a0f0e
HiddenInNet <dgongar3112@gmail.com>
2026-04-23 22:46:46 +0000

    fix: arregla vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   i18n: {
     locales: ["es", "en", "fr"],
     defaultLocale: "es",
     routing: {
         prefixDefaultLocale: true
     }
-  }
+  },
+  output: 'server',
+  adapter: vercel()
 });
